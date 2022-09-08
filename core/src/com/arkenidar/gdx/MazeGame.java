515c62d2e5711a0a122da5010cd7c567fddc7d67
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-09-08 23:44:18 +0200

    grid-based movement

--- core/src/com/arkenidar/gdx/MazeGame.java
+++ core/src/com/arkenidar/gdx/MazeGame.java
@@ -8,40 +8,41 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MazeGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture space,wall,player,end;
-	float x=0,y=0;
+    SpriteBatch batch;
+    Texture space, wall, player, end;
+    float x = 0, y = 0;
+    final float spriteSize = 32;
 
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		// from GNU-Robot
-		space = new Texture("-.bmp");
-		wall = new Texture("W.bmp");
-		player = new Texture("P.bmp");
-		end = new Texture("E.bmp");
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        // from GNU-Robot (size: 32x32)
+        space = new Texture("-.bmp");
+        wall = new Texture("W.bmp");
+        player = new Texture("P.bmp");
+        end = new Texture("E.bmp");
+    }
 
-	@Override
-	public void render () {
+    @Override
+    public void render() {
 
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) x -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) x += 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) y -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.UP)) y += 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) x--;
+        if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) x++;
+        if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) y--;
+        if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) y++;
 
-		ScreenUtils.clear(0, 1, 0, 1);
-		batch.begin();
-		batch.draw(player, x, y);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		player.dispose();
-		space.dispose();
-		wall.dispose();
-		end.dispose();
-	}
+        ScreenUtils.clear(0, 1, 0, 1);
+        batch.begin();
+        batch.draw(player, x*spriteSize, y*spriteSize);
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        player.dispose();
+        space.dispose();
+        wall.dispose();
+        end.dispose();
+    }
 }
